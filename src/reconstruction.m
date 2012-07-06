2ce1c0d782885239b9d47c098e08113db9f992a5
koos.krisztian <koos.krisztian@gmail.com>
2012-07-06 15:44:32 +0000

    diffusion

---
+++ src/reconstruction.m
@@ -0,0 +1,23 @@
+function result=reconstruction(mask, marker)
+    previousResult = zeros(size(marker, 1), size(marker, 2));
+    result = marker;
+
+    %figure('name', 'Reconstruction Mask'), imshow(mask);
+    %figure('name', 'Reconstruction Marker'), imshow(marker);
+    %figure('name', 'Reconstruction initial "result"'), imshow(result);
+    %figure('name', 'Reconstruction initial "previous result"'), imshow(previousResult);
+    
+    
+    reconstructionIteration = 0;
+    %figure('name', 'Reconstruction Step');
+    while ~(isequal(result, previousResult))
+        reconstructionIteration = reconstructionIteration + 1
+        previousResult = result;
+        result = imdilate(result, [0 1 0; 1 1 1; 0 1 0]);
+        result = min(result, mask);
+
+        %if mod(reconstructionIteration, 16) == 0 
+        %  imshow(result);        
+        %end
+    end   
+end
